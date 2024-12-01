2c400857bd104a80072be05b7e28e37123d887ef
Abbath <pitongogi@gmail.com>
2024-12-01 14:24:17 +0100

    Optimization.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
+use std::fs;
 use std::time::Instant;
 
 fn day_01() {
-    let file = File::open("input/input_01.txt").unwrap();
-    let reader = BufReader::new(file);
-    let lines: Vec<String> = reader.lines().flatten().collect();
-    let (mut left, mut right): (Vec<_>, Vec<_>) = lines
-        .iter()
+    let (mut left, mut right): (Vec<_>, Vec<_>) = fs::read_to_string("input/input_01.txt")
+        .unwrap()
+        .lines()
         .map(|line| {
             let parts: Vec<_> = line
                 .split_whitespace()
@@ -25,8 +22,8 @@ fn day_01() {
         .map(|(a, b)| (a - b).abs())
         .sum();
     let mut hm: HashMap<i64, i64> = HashMap::new();
-    right.iter().for_each(|n| {
-        *hm.entry(*n).or_default() += 1;
+    right.iter().for_each(|&n| {
+        *hm.entry(n).or_default() += 1;
     });
     let sum2: i64 = left.iter().map(|n| n * hm.get(n).unwrap_or(&0)).sum();
     println!("day01 {sum} {sum2}");
