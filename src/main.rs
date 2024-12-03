0ef70fbbf7f9525c2e980802617c356b065e2b1a
Abbath <pitongogi@gmail.com>
2024-12-03 14:37:14 +0100

    Day 04.

--- src/main.rs
+++ src/main.rs
@@ -79,7 +79,115 @@ fn day_02() {
     println!("day02 {sum} {sum2}");
 }
 
+fn day_03() {
+    let lines: Vec<String> = fs::read_to_string("input/input_03.txt")
+        .unwrap()
+        .lines()
+        .map(|l| l.to_string())
+        .collect();
+    fn compute(line: &str, enabling: bool, old_enabled: bool) -> (u64, bool) {
+        enum State {
+            S,
+            D,
+            M,
+            D1,
+            C,
+            D2,
+            RP,
+        }
+        let mut state = State::S;
+        let mut idx = 0usize;
+        let mut d1s: Vec<char> = Vec::new();
+        let mut d2s: Vec<char> = Vec::new();
+        let mut sum = 0u64;
+        let mut enabled = old_enabled;
+        loop {
+            if idx >= line.len() {
+                break;
+            }
+            let c = line.chars().nth(idx).unwrap();
+            match state {
+                State::S => {
+                    if (!enabling || enabled) && c == 'm' {
+                        idx -= 1;
+                        state = State::M;
+                    }
+                    if enabling && c == 'd' {
+                        idx -= 1;
+                        state = State::D;
+                    }
+                }
+                State::D => {
+                    if idx + 4 <= line.len() && line[idx..idx + 4] == *"do()" {
+                        idx += 3;
+                        enabled = true;
+                    }
+                    if idx + 7 <= line.len() && line[idx..idx + 7] == *"don't()" {
+                        idx += 6;
+                        enabled = false;
+                    }
+                    state = State::S;
+                }
+                State::M => {
+                    if idx + 4 <= line.len() && line[idx..idx + 4] == *"mul(" {
+                        idx += 3;
+                        state = State::D1;
+                    } else {
+                        state = State::S;
+                    }
+                }
+                State::D1 => {
+                    if c.is_ascii_digit() {
+                        d1s.push(c);
+                    } else {
+                        idx -= 1;
+                        state = State::C;
+                    }
+                }
+                State::C => {
+                    if c == ',' {
+                        state = State::D2;
+                    } else {
+                        d1s.clear();
+                        state = State::S;
+                    }
+                }
+                State::D2 => {
+                    if c.is_ascii_digit() {
+                        d2s.push(c)
+                    } else {
+                        idx -= 1;
+                        state = State::RP;
+                    }
+                }
+                State::RP => {
+                    if c == ')' {
+                        let d1: u64 = d1s.iter().collect::<String>().parse().unwrap();
+                        let d2: u64 = d2s.iter().collect::<String>().parse().unwrap();
+                        sum += d1 * d2;
+                    }
+                    d1s.clear();
+                    d2s.clear();
+                    state = State::S;
+                }
+            }
+            idx += 1;
+        }
+        (sum, enabled)
+    }
+    let sum: u64 = lines.iter().map(|l| compute(l, false, true).0).sum();
+    let sum2: u64 = lines
+        .iter()
+        .fold((0u64, true), |(s, e), l| {
+            let (ns, ne) = compute(l, true, e);
+            (s + ns, ne)
+        })
+        .0;
+    println!("day03 {sum} {sum2}");
+}
+
 fn main() {
     day_01();
     day_02();
+    day_03();
 }
